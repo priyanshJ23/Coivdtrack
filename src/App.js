6a2404c3fa7a246d7fc7453d92f5113ad2669ace
2306-priyansh <78022628+2306-priyansh@users.noreply.github.com>
2022-05-25 17:21:00 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Cards, CountryPicker, Chart } from "./Components";
+import { fetchData } from "./api/";
+import styles from "./App.module.css";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
